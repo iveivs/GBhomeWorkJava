bcf217dadebe175b81a963443307416a4a10f12a
veiv <veiv@mail.ru>
2022-11-23 02:52:53 +0400

    add hw1_task_1

---
+++ hwJ_1/hw1_task_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sem1_task_1 {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+
+        System.out.printf("int n: ");
+
+        int x = iScanner.nextInt();
+
+        int mult = 1;
+
+        for (int i = 1; i <= x; i++) {
+            mult = mult * i;
+        }
+
+        System.out.println(mult);
+
+        iScanner.close();
+
+
+    }
+}
