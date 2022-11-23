e8d73e3d350ee267b8320a4ed5b2e6db4d6b2fdd
veiv <veiv@mail.ru>
2022-11-23 05:37:01 +0400

    add hw1_task_3

--- hwJ_1/hw1_task_3.java
+++ hwJ_1/hw1_task_3.java
@@ -2,8 +2,48 @@ import java.util.Scanner;
 
 public class hw1_task_3 {
     public static void main(String[] args) {
+
+        Scanner xScanner = new Scanner(System.in);
+
+        System.out.printf("input x: ");
+
+        int x = xScanner.nextInt();
+
+
+        Scanner yScanner = new Scanner(System.in);
+
+        System.out.printf("input y: ");
+
+        int y = yScanner.nextInt();
+
+
+        Scanner signScanner = new Scanner(System.in);
+
+        System.out.printf("input operation: ");
+
+        String sign = signScanner.nextLine();
+
         
+
+        if (sign == "+") {
+            int res = 0;
+            res = x + y;
+            System.out.println(res);
+        }
+        else if (sign == "-") {
+            int res = 0;
+            res = x - y;
+            System.out.println(res);
+        }
+        else if (sign == "*") {
+            int res = 0;
+            res = x * y;
+            System.out.println(res);
+        }
+        else if (y != 0 && sign == "/") {
+            int res = 0;
+            res = x / y;
+            System.out.println(res);
+        }
     }
-    
-    
 }
