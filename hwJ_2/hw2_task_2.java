77a09a18df3650087defa9e8a991b00e361fcd22
veiv <veiv@mail.ru>
2022-11-29 02:59:54 +0300

    add hw 2 t 2

---
+++ hwJ_2/hw2_task_2.java
@@ -0,0 +1,24 @@
+package hwJ_2;
+import java.util.Arrays;
+
+public class hw2_task_2 {
+    public static void main(String[] args) {
+        int [] mas = {11, 3, 14, 16, 7};
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+                    
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
